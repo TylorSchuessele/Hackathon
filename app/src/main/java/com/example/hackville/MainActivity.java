37516ccd55fe33325f41909275c0104107c1c838
Nicholas Sharpe <nsharpe.4737@gmail.com>
2019-02-09 12:07:26 -0500

    + Added container for fragments to main_activity.xml + Added example for displayng a fragment to MainActivity + Created layout file for text to speech fragment + Created text to speech fragment

--- app/src/main/java/com/example/hackville/MainActivity.java
+++ app/src/main/java/com/example/hackville/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.hackville;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static FragmentManager fragmentManager;
+    public LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        if(findViewById(R.id.container) != null) {
+
+            if(savedInstanceState != null) {
+                return;
+            }
+        }
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        TextToSpeechFragment textToSpeechFragment = new TextToSpeechFragment();
+        fragmentTransaction.add(R.id.container, textToSpeechFragment, null);
+        fragmentTransaction.commit();
+
     }
 }
