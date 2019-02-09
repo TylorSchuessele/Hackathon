b44caec345a8f29327bcbf8bfada433ba1378fff
Tylor Schuessele <schuesse@sheridancollege.ca>
2019-02-09 10:55:32 -0500

    Created Activities for App

---
+++ app/src/main/java/com/example/hackville/ChooseTopicActivity.java
@@ -0,0 +1,13 @@
+package com.example.hackville;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseTopicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_topic);
+    }
+}
